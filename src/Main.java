f24c8b8f66eb1e5af1387db7d3d020711b37b75a
akira <Mich.MX0098@gmail.com>
2020-05-02 23:02:50 +0300

    Create calculator

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class Calculator {
+    // Объявили метод mein() не возврощает данные
+    public static void main(String[] args) {
+
+        Scanner num = new Scanner(System.in); // class сканер, сканирует входные данные, более точная инфа о (System.in); ( http://tutorials.jenkov.com/java-io/system-in-out-error.html )
+
+        int first, second;  // объявил переменную с плавоющей точкой 4б
+
+        System.out.println("Enter first num: ");// Выводим в консоль
+        first = num.nextInt(); // Читает значение int от пользователя
+
+        System.out.println("Enter second num: ");
+        second = num.nextInt();
+
+        System.out.println("Enter Charapter");
+        char character = num.next().charAt(0); // class Scaner не подерживает num.nextChar() Чтобы прочитать символ, мы используем next (). CharAt (0)
+	
+	int result = 0;
+
+	    if(character == '+'){
+            result = first + second;
+        }
+
+        if(character == '-'){
+            result = first - second;
+        }
+
+        if(character == '/'){
+            result = first / second;
+        }
+
+        if(character == '*'){
+            result = first * second;
+        }
+
+        System.out.println("Вот ну как бы Нащайника я насчитал как то так   " + result);
+    }
+}
+
+
+
+
